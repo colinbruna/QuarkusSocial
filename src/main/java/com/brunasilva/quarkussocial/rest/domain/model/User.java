ba13dfd7c83dd419977cebab18cdc0c9313a1481
colinbruna <brunacbaldin@gmail.com>
2022-08-28 17:48:50 -0300

    codigos de status

--- src/main/java/com/brunasilva/quarkussocial/rest/domain/model/User.java
+++ src/main/java/com/brunasilva/quarkussocial/rest/domain/model/User.java
@@ -1,53 +1,17 @@
 package com.brunasilva.quarkussocial.rest.domain.model;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
+import lombok.Data;
 import org.bson.types.ObjectId;
 
 import javax.persistence.Id;
-import java.util.Objects;
 
 @MongoEntity(collection = "user")
+@Data
 public class User{
 
     @Id
     private ObjectId id;
     private String name;
     private Integer age;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 }
--- src/main/java/com/brunasilva/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/com/brunasilva/quarkussocial/rest/dto/ResponseError.java
@@ -1,12 +1,18 @@
 package com.brunasilva.quarkussocial.rest.dto;
 
+import lombok.Data;
+
 import javax.validation.ConstraintViolation;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
 public class ResponseError {
+
+    public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
 
     private String message;
     private Collection<FieldError> errors;
@@ -28,19 +34,7 @@ public class ResponseError {
         return responseError;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Collection<FieldError> getErrors() {
-        return errors;
-    }
-
-    public void setErrors(Collection<FieldError> errors) {
-        this.errors = errors;
+    public Response withStatusCode(int code) {
+        return Response.status(code).entity(this).build();
     }
 }
